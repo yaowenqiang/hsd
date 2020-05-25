d03ce6b7db4188877e88c54e27e17f0b44e2c05a
yaowenqiang <yaowenqiang111@163.com>
2020-05-26 01:40:58 +0800

    add type synonyms demo

--- Hello.hs
+++ Hello.hs
@@ -594,3 +594,34 @@ showSum :: (Num a, Show a) => [a] -> [Char]
 showSum xs = show (sum xs)
 3.1 :: Fractional p => p -- 分类
 
+
+-- Custom Types
+--
+
+-- Type Synonyms(同意词)
+--type String = [Char
+--type Point = (Double, Double)
+--
+-- Newtype
+-- Records
+-- Algebraic(代数)) Data Types
+
+
+midpoint :: (Double, Double) -> (Double, Double)  -> (Double, Double)
+
+midpoint (x1, y1) (x2, y2) = 
+    ((x1 + x2) / 2, (y1 + y2) /2)
+
+
+midpoint' :: Point -> Point  -> Point
+
+midpoint (x1, y1) (x2, y2) = 
+    ((x1 + x2) / 2, (y1 + y2) /2)
+
+p1 :: (Double, Double)
+p1 = (1,2)
+p2 :: Point
+p2 = (3,4)
+
+mid :: (Double, Double)
+mid = midpoint p1 p2
